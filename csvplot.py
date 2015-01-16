26d30a91db75c04caee760694c53f21c678b38b4
Benjamin Brent <benjamin@wnproject.com>
2015-01-16 12:15:32 +1100

    Update docs

--- csvplot.py
+++ csvplot.py
@@ -1,9 +1,10 @@
 from matplotlib import pyplot
 
 # Plot given X and Y series, saving output to given file path
-#   Series is a list of dicts in the form
+#   series: list of dicts in the form
 #     {'x': list, 'y': list, 'series': str}
-#   outputfile is the path to the output file
-#   xaxis and yaxis are the x and y axis labels
-def plot(series, outputfile, xaxis="", yaxis=""):
+#   outputfile: path to the output file
+#   xaxis and yaxis: the x and y axis labels
+#   regression: boolean indicating whether to plot a linear regression line
+def plot(series, outputfile, xaxis="", yaxis="", regression=True):
     pass
