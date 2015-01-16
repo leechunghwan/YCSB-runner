3c8d3758db317cfd5e217b244ee1a2bb93f4ba69
Benjamin Brent <benjamin@wnproject.com>
2015-01-16 12:10:27 +1100

    Add comment

--- csvplot.py
+++ csvplot.py
@@ -1,3 +1,9 @@
-# TODO: Plots YCSB+T data nicely
-def plot():
+from matplotlib import pyplot
+
+# Plot given X and Y series, saving output to given file path
+#   Series is a list of dicts in the form
+#     {'x': list, 'y': list, 'series': str}
+#   outputfile is the path to the output file
+#   xaxis and yaxis are the x and y axis labels
+def plot(series, outputfile, xaxis="", yaxis=""):
     pass
