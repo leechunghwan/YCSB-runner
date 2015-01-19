36abf9e97d5c8611f713ae1b0e7f55ee23abe8f3
Benjamin Brent <benjamin@wnproject.com>
2015-01-19 12:31:52 +1100

    Remove testplot, enable redis transactions in test workload config

--- testplot.py
+++
@@ -1,6 +0,0 @@
-from plots import scatter, series
-
-scatter([{'x':[1,2,3], 'y':[8,16,32], 'series':'base2'}, {'x':list(range(6)),
-'y':[1,2,4,8,16,32], 'series': 'testb2'}, {'x':list(range(6)),
-'y':list(range(6)), 'series':'testlinear'}], 'blah.pdf', xaxis="Testx",
-yaxis="Testy")
