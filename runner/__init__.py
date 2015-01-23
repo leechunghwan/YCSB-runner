ecc9c90d9c037d34c12aea122702961b929760df
Benjamin <benjamin@benjaminbrent.com>
2015-01-23 13:07:43 +1100

    Add comment to runner init

--- runner/__init__.py
+++ runner/__init__.py
@@ -1,3 +1,5 @@
+# We should only import what we need to avoid pollution in the global
+# namespace
 from .dbsystem import DbSystem
 from .config   import RunnerConfig
 from .stats    import Statistics, StatisticsSet
