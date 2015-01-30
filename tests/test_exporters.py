dc7863f678997a9c2ba20e459ccc449b5e0992ea
Benjamin <benjamin@benjaminbrent.com>
2015-01-30 18:11:04 +1100

    Don't use hard-coded strings from constants.py in exporters tests

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -3,6 +3,8 @@ import tempfile
 import unittest
 from time import time
 
+from .helpers import *
+
 import runner.constants as const
 from runner.exporter     import Exporter
 from runner.csv_exporter import CsvExporter
@@ -28,6 +30,7 @@ class CsvExporterTestCase(unittest.TestCase):
     def setUp(self):
         self.ss = StatisticsSet(Statistics(), Statistics())
         self.exp = CsvExporter(self.ss)
+        self.statkey1, self.statkey2 = getlist(const.TRACKED_STATS.keys(), 2)
 
     def test_init(self):
         self.assertEqual(self.exp.stats_set, self.ss)
@@ -36,19 +39,19 @@ class CsvExporterTestCase(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmpdirname:
             fname = os.path.join(tmpdirname, 'export.csv')
             self.assertFalse(os.path.exists(fname))
-            self.exp.export(fname, 'mpl', 'runtime')
+            self.exp.export(fname, self.statkey1, self.statkey2)
             self.assertTrue(os.path.exists(fname))
 
     def test_export_averages(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             fname = os.path.join(tmpdirname, 'averages.csv')
             self.assertFalse(os.path.exists(fname))
-            self.exp.export_averages(fname, 'mpl', 'runtime')
+            self.exp.export_averages(fname, self.statkey1, self.statkey2)
             self.assertTrue(os.path.exists(fname))
 
     def test_export_averages_plot(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             fname = os.path.join(tmpdirname, 'plot.pdf')
             self.assertFalse(os.path.exists(fname))
-            self.exp.export(fname, 'testplot', 'mpl', 'runtime')
+            self.exp.export(fname, 'testplot', self.statkey1, self.statkey2)
             self.assertTrue(os.path.exists(fname))
