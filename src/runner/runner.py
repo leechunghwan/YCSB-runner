273f9b3c481ea3f4e320c0f5c7691a352faed62c
Benjamin <benjamin@benjaminbrent.com>
2015-01-31 23:39:27 +1100

    Move 'starting trial' log string to actual start of trial

--- src/runner/runner.py
+++ src/runner/runner.py
@@ -41,12 +41,12 @@ class Runner:
             copyfile(db.workload_path, db.makefpath("workload-{}-{}"))
             for trial in range(1, db.trials + 1):
                 self.__run_hooks("PRE_TRIAL", trial, db)
+                db.log("Starting trial %i..." % (trial), mpl=mpl, trial=trial)
                 for mpl in count(start=db.min_mpl, step=db.inc_mpl):
                     self.__run_hooks("PRE_MPL", mpl, trial, db)
                     # Obvious; don't go above configured maximum MPL
                     if mpl > db.max_mpl:
                         break
-                    db.log("Starting trial %i..." % (trial), mpl=mpl, trial=trial)
                     # Clean the database
                     db.log("Cleaning the database...", mpl=mpl, trial=trial)
                     db.clean()
