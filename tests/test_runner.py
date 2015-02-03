2f186523fa59296eafb820cf1fa3abd76a12d343
Benjamin <benjamin@benjaminbrent.com>
2015-02-03 14:02:36 +1100

    Initial test case for runner

---
+++ tests/test_runner.py
@@ -0,0 +1,19 @@
+from .helpers import *
+
+import runner.constants as const
+from runner.runner import Runner
+
+class RunnerTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
